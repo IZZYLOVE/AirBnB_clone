ccf5bf99f9aa6fb45da4ae736fd0de7ce532d5a6
Gabtoriel <gabtorielvixgmail.com>
2023-05-11 08:08:18 +0000

    task 4 of the hbnb project console modified the base class init function to accept tuple and dictionary objects though the tuples isn't in use

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,16 @@
 """This is the base model of all objects in the Airbnb web project.
 
 
-   Description: The base_model of the Hbnb project, "a clone of the 
-   Airbnb website" is a file that contains the programming 
+   Description: The base_model of the Hbnb project, "a clone of the
+   Airbnb website" is a file that contains the programming
    construct to create a class that in turn create objects that will
    be used to create and control the workings of the hbnb website.
 """
 
 import datetime
 import uuid
+import re
+
 
 class BaseModel:
 
@@ -26,28 +28,51 @@ class BaseModel:
        @updated_at: Holds the date and time of a newly updated instance object.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
 
         """Initializes the instances objects of the base class."""
 
-	self.id = str(uuid.uuid4())
-	self.created_at = datetime.datetime.now()
-	self.updated_at = datetime.datetime.now()
+        if kwargs is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                if k == "created_at":
+                    delimeters = r"[-.:T]"
+                    d = re.split(delimeters, v)
+                    self.k = datetime.datetime(int(d[0]), int(d[1]), int(d[2]),
+                                               hour=int(d[3]),
+                                               minute=int(d[4]),
+                                               seconds=int(d[5]),
+                                               microsecond=int(d[6]))
+                if k == "updated_at":
+                    delimeters = r"[-.:T]"
+                    d = re.split(delimeters, v)
+                    self.k = datetime.datetime(int(d[0]), int(d[1]), int(d[2]),
+                                               hour=int(d[3]),
+                                               minute=int(d[4]),
+                                               seconds=int(d[5]),
+                                               microsecond=int(d[6]))
+                self.k = v
 
     def __str__(self):
 
         """Returns the string representation of the object."""
 
-        hbnb_Bobj_str = "[{}] ({}) {}".format("BaseModel", self.id, self.__dict__)
-	return hbnb_Bobj_str
+        hbnb_Bobj_str = "[{}] ({}) {}".format("BaseModel",
+                                              self.id, self.__dict__)
+        return hbnb_Bobj_str
 
     def save(self):
 
         """Updates the public instance variable updated_at to the current date
-	   time object.
+           time object.
         """
 
-	self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
 
