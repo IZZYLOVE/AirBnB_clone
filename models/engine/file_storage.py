498592d463c5c3cba015b731a2844028180883f9
Gabtoriel <gabtorielvixgmail.com>
2023-05-14 16:00:49 +0000

    removes whitespaces from the file and made it whitespaces compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """Defines the FileStorage class."""
+
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -11,32 +14,43 @@ from models.review import Review
 
 
 class FileStorage:
+
     """Represents an abstracted storage engine.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+
         """Returns the dictionary __objects."""
+
         return FileStorage.__objects
 
     def new(self, obj):
+
         """Set in __objects obj with key <obj_class_name>.id"""
+
         ocname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
+
         """Serializes __objects to the JSON file __file_path."""
+
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
     def reload(self):
-        """Deserializes the JSON file __file_path to __objects, if it exists."""
+
+        """Deserializes the JSON file __file_path to __objects, if it exists.
+        """
+
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
